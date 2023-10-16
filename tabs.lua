aec422d3b736a72d1916090be004f20aa2d49424
1o1vfx <147369616+1o1vfx@users.noreply.github.com>
2023-10-15 23:40:04 -0400

    Add files via upload

---
+++ tabs.lua
@@ -0,0 +1,35 @@
+spawnmenu.AddCreationTab( "Duplicator.gg", function() -- Replace Duplicator.gg with the name of your tab EX - yourserver - forums
+    local html = vgui.Create("DHTML", frame)
+    html:Dock(FILL)
+    html:OpenURL("Put your link here")
+    return html
+
+
+end, "icon16/control_repeat_blue.png", 200 ) -- Icon
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+spawnmenu.AddCreationTab( "Duplicator.gg", function() -- Replace Duplicator.gg with the name of your tab EX yourserver - store
+    local html = vgui.Create("DHTML", frame)
+    html:Dock(FILL)
+    html:OpenURL("Put your link here")
+    return html
+
+
+end, "icon16/control_repeat_blue.png", 200 ) -- Icon
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+--====== NEEDS CHROMIUM TO SEE
+
+-- How to enable chromium
+
+-- Right click Garry's Mod on Steam
+
+-- Go to Properties
+
+-- Go to Betas
+
+-- Select the 86-64 Bit Chromium 
+
+-- Update your game and your game will be chromium
+
+----- This will also make your game run better in general and always recommended
+
+-- Author 1o1vfx @ Duplicator.gg
